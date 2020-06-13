0b9f699fa04050278d09c68e7f70a978c20d055e
Milton <e0267381@u.nus.edu>
2020-06-13 14:51:46 +0800

    Added MySQL ORM and api routes

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,10 @@
+const apiRoutes = require('./routes/apiRoutes');
+
 module.exports = function(app) {
+
+    apiRoutes(app);
 
     app.get('/', function(req, res) {
         res.send('Hello World!')
     });
- 
-    // BOARD SECTION =========================
-    app.get('/endpoint1', async(req, res)=> {
-        res.send('Hello endpoint1!')
-    });
-
-
-   app.get('/endpoint2', async(req, res)=> {
-        res.send('Hello endpoint2!')
-   });
-
-
-   app.get('/endpoint3', async(req, res)=> {
-        res.send('Hello endpoint3!')
-   });
-
-}
+};
---
+++ app/routes/apiRoutes.js
@@ -0,0 +1,37 @@
+
+module.exports = function(app) {
+
+    //Test 1: List Teachers
+    app.get('/api/getteachers', async(req, res) => {
+        
+    });
+
+    //Test 2: Set teachers
+    app.post('/api/setteachers', async(req, res) => {
+        res.send('US1!')
+    });
+
+
+
+    //User story 1
+    app.post('/api/register', async(req, res) => {
+        res.send('US1!')
+    });
+
+    //User Story 2
+    app.get('/api/commonstudents', async(req, res) => {
+        res.send('US2!')
+    });
+
+    //User Story 3
+    app.post('/api/suspend', function(req, res) {
+        res.send('US3!')
+    });
+ 
+    //User Story 4
+    app.post('/api/retrievefornotifications', function(req, res) {
+        res.send('US4!')
+    });
+
+
+}
